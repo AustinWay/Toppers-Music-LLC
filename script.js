da82bb4f521e567d99e4c3050a3902b74374f3c5
AustinWay <AustinWay@biggexpress.com>
2020-07-27 11:29:42 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,27 @@
+/*Gets year for footer*/
+window.onload = function() {
+    var d = new Date();
+    var n = d.getFullYear();
+    document.getElementById("getYear").innerHTML = n;
+};
+
+function instrumentsPage() {
+    window.location.href = "electric_guitars_and_amps.html";
+};
+
+function instructionPage() {
+    window.location.href = "instruction_and_staff.html";
+};
+
+function contactPage() {
+    window.location.href = "contact.html";
+};
+
+function instagramPage() {
+    window.location.href = "https://www.instagram.com/toppersmusic/";
+};
+
+function facebookPage() {
+    window.location.href = "https://www.facebook.com/Toppers-Music-LLC-235623512511/";
+};
+
